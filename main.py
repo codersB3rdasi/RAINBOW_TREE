3549a01cfe7fe70ae6d90dd4683a21051bfbdd8f
unknown <codersb3rdasi@gmail.com>
2024-03-01 15:57:47 +0800

    Tree Turle

---
+++ main.py
@@ -0,0 +1,31 @@
+import turtle
+import colorsys
+
+def draw_stick(x, y, length, pensize, color, angle):
+    turtle.up()
+    turtle.goto(x,y)
+    turtle.seth(angle)
+    turtle.pensize(pensize)
+    turtle.down()
+    turtle.color(color)
+    turtle.fd(length)
+
+def draw_tree(x, y, length, pensize, hue, angle, fat_angle, n):
+    if n == 0:
+        return
+    (r,g,b) = colorsys.hsv_to_rgb(hue, 1, 1)
+    draw_stick(x, y, length, pensize, (r,g,b), angle)
+    tx = turtle.xcor()
+    ty = turtle.ycor()
+
+    draw_tree(tx, ty, length * 0.7, pensize * 0.7, hue - 1/13, angle + fat_angle, fat_angle, n - 1)
+    draw_tree(tx, ty, length * 0.7, pensize * 0.7, hue - 1/13, angle - fat_angle, fat_angle, n - 1)
+
+turtle.setup(800,700)
+turtle.title("Rainbow Colored Tree - @CodersB3rdasi")
+turtle.speed(0)
+turtle.tracer(n=1, delay=0)
+turtle.bgcolor("black")
+
+draw_tree(0, -300, 200, 10, 12 / 13, 90, 25, 12)
+turtle.done()
